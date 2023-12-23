4f2e36aa38953219c8e8088b8330eeb20e7f8295
Ivo Donev <ivodonev04@gmail.com>
2023-12-23 02:23:18 +0100

    Added a pandas library function where the program now also creates a csv file with the data for an easier reading experience.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from prettytable import PrettyTable
 
 
@@ -11,15 +12,15 @@ def prioritize_subjects(subjects_scores):
     return lowest_subjects
 
 
-def write_to_score_file(file_number, nbu_test_score, matura_1_score, matura_2_score, final_score, needed_top_score, subjects_scores, prioritized_subjects):
+def write_to_txt(file_number, NBU_test_score, Matura_1_score, Matura_2_score, Final_score, needed_Top_score, subjects_scores, prioritized_subjects):
     new_filename = f"Scores_{file_number}.txt"
     with open(new_filename, "w", encoding="utf-8") as file:
         file.write("Scores and Information:\n")
-        file.write(f"Top Test Score: {nbu_test_score}\n")
-        file.write(f"Matura 1 Score: {matura_1_score}\n")
-        file.write(f"Matura 2 Score: {matura_2_score}\n")
-        file.write(f"Final Score For Entering NBU: {final_score}\n")
-        file.write(f"Points needed next time for the Top Test: {needed_top_score}\n")
+        file.write(f"Top Test Score: {NBU_test_score}\n")
+        file.write(f"Matura 1 Score: {Matura_1_score}\n")
+        file.write(f"Matura 2 Score: {Matura_2_score}\n")
+        file.write(f"Final Score For Entering NBU: {Final_score}\n")
+        file.write(f"Points needed next time for the Top Test: {needed_Top_score}\n")
 
         file.write("\nScores for Subjects:\n")
         for subject, score in subjects_scores.items():
@@ -31,22 +32,45 @@ def write_to_score_file(file_number, nbu_test_score, matura_1_score, matura_2_sc
     print(f"File '{new_filename}' created successfully.")
 
 
-def nbu_calculator():
+def write_to_csv(file_number, NBU_test_score, Matura_1_score, Matura_2_score, Final_score, needed_Top_score, subjects_scores, prioritized_subjects):
+    data = {
+        'Top Test Score': [NBU_test_score],
+        'Matura 1 Score': [Matura_1_score],
+        'Matura 2 Score': [Matura_2_score],
+        'Final Score For Entering NBU': [Final_score],
+    }
+
+    df = pd.DataFrame(data)
+    df['Points you need next time you do the Top Test'] = [needed_Top_score]
+
+    subjects = ["Бълг.", "Лит.", "Ист.", "Георг.", "Матем.", "Физ.", "Хим.", "Биол.", "Разс.", "Сем."]
+    for subject in subjects:
+        df[subject] = [subjects_scores.get(subject, '')]
+
+    prioritized_subjects_str = ', '.join(prioritized_subjects)
+    df['Subjects that need improvement'] = [prioritized_subjects_str]
+
+    csv_filename = f"Scores_{file_number}.csv"
+    df.to_csv(csv_filename, index=False, encoding="utf-8")
+    print(f"File '{csv_filename}' created successfully.")
+
+
+def NBU_Calculator():
     print("Welcome to NBU score calculator, please prepare your NBU TOP score and matura grades.")
 
-    nbu_test_score = float(input("How many points did you get at the TOP test? "))
-    matura_1_score = float(input("What grade did you get at the first matura test? "))
-    matura_2_score = float(input("What grade did you get at the second matura test? "))
+    NBU_test_score = float(input("How many points did you get at the TOP test? "))
+    Matura_1_score = float(input("What grade did you get at the first matura test? "))
+    Matura_2_score = float(input("What grade did you get at the second matura test? "))
 
-    matura_score = ((matura_1_score + matura_2_score) / 2) - 2
-    matura_score_for_nbu = (75 * matura_score)
-    final_score = round((7 * nbu_test_score) + matura_score_for_nbu)
+    Matura_score = ((Matura_1_score + Matura_2_score) / 2) - 2
+    Matura_score_for_NBU = (75 * Matura_score)
+    Final_score = round((7 * NBU_test_score) + Matura_score_for_NBU)
 
     score_table = PrettyTable()
-    score_table.add_column("Top Test Score", [nbu_test_score])
-    score_table.add_column("Matura 1 Score", [matura_1_score])
-    score_table.add_column("Matura 2 Score", [matura_2_score])
-    score_table.add_column("Final Score For Entering NBU", [final_score])
+    score_table.add_column("Top Test Score", [NBU_test_score])
+    score_table.add_column("Matura 1 Score", [Matura_1_score])
+    score_table.add_column("Matura 2 Score", [Matura_2_score])
+    score_table.add_column("Final Score For Entering NBU", [Final_score])
 
     print(score_table)
 
@@ -54,10 +78,10 @@ def nbu_calculator():
 
     if answer == "no":
         needed_score = int(input("And how many overall points do you need/want to have? "))
-        points_after_calculation = (needed_score - matura_score_for_nbu)
-        needed_top_score = int(points_after_calculation / 7)
+        points_after_calculation = (needed_score - Matura_score_for_NBU)
+        needed_Top_score = int(points_after_calculation / 7)
 
-        score_table.add_column("Points you need next time you do the Top Test", [needed_top_score])
+        score_table.add_column("Points you need next time you do the Top Test", [needed_Top_score])
         print(score_table)
 
         subjects = ["Бълг.", "Лит.", "Ист.", "Георг.", "Матем.", "Физ.", "Хим.", "Биол.", "Разс.", "Сем."]
@@ -72,7 +96,7 @@ def nbu_calculator():
 
         # Find the latest numbered Scores file in the directory
         current_dir = os.getcwd()
-        existing_files = [f for f in os.listdir(current_dir) if f.startswith("Scores_") and f.endswith(".txt")]
+        existing_files = [f for f in os.listdir(current_dir) if f.startswith("Scores_") and (f.endswith(".csv") or f.endswith(".txt"))]
 
         if existing_files:
             latest_number = max(int(file.split("_")[1].split(".")[0]) for file in existing_files)
@@ -80,13 +104,14 @@ def nbu_calculator():
         else:
             new_file_number = 1
 
-        # Write scores and information to the file
-        write_to_score_file(new_file_number, nbu_test_score, matura_1_score, matura_2_score, final_score, needed_top_score, subjects_scores, prioritized_subjects)
+        # Write scores and information to both CSV and TXT files using the respective functions
+        write_to_csv(new_file_number, NBU_test_score, Matura_1_score, Matura_2_score, Final_score, needed_Top_score, subjects_scores, prioritized_subjects)
+        write_to_txt(new_file_number, NBU_test_score, Matura_1_score, Matura_2_score, Final_score, needed_Top_score, subjects_scores, prioritized_subjects)
 
     else:
         print("Have a great time in NBU next year then!")
 
 
 # Run the program
-nbu_calculator()
+NBU_Calculator()
 input()
